9234b26d2ba68a854ce0845b14a98fe8446c0489
Thom Barrett <thom440@yahoo.com>
2021-02-01 14:40:21 -0800

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a single checking account
+    /// </summary>
+    public class Account
+    {
+        private double _balance;
+
+        /// <summary>
+        /// Deposits the amount in the bank account and 
+        /// returns the new balance
+        /// </summary>
+        /// <param name="amount">The amount to deposit</param>
+        public double Deposit(double amount)
+        {
+            _balance += amount;
+            return _balance;
+        }
+
+        /// <summary>
+        /// Get the current balance
+        /// </summary>
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+
+        }
+    }
+}
